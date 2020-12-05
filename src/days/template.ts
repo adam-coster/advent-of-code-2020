acf8cabb9f953efd13fbd9526f41acd51e98b602
Adam Coster <adam@bscotch.net>
2020-12-05 17:14:40 -0600

    feat: Create template Typescript script for new days.

---
+++ src/days/template.ts
@@ -0,0 +1,37 @@
+/**
+
+ */
+
+import { assert, splitOnLinebreak, countBy, cumulativeSum } from "../lib/utility";
+import {Day} from "../types/Day";
+
+function parseDataset (string:string){
+  const lines = splitOnLinebreak(string);
+  return lines;
+}
+
+/** Tally the number of VALID passwords */
+function puzzle1(dataset:string){
+  const data = parseDataset(dataset);
+  return Infinity;
+}
+
+function puzzle2(dataset:string){
+  const data = parseDataset(dataset);
+  return Infinity;
+}
+
+const day: Day = {
+  day: Infinity,
+  sample:{
+    input: `
+    
+    `,
+    puzzle1: Infinity,
+    puzzle2: Infinity
+  },
+  puzzle1,
+  puzzle2,
+};
+
+export default day;
