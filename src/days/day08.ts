7e14a4b3472d9b20ac7939ed6698fd1b38f546cc
Adam Coster <adam@bscotch.net>
2020-12-12 15:33:56 -0600

    feat: Complete Day 8 Puzzle 2.

--- src/days/day08.ts
+++ src/days/day08.ts
@@ -1,7 +1,8 @@
 /** @see https://adventofcode.com/2020/day/8 */
 
-import { assert, splitOnLinebreak, countBy, cumulativeSum } from "../lib/utility";
+import { splitOnLinebreak } from "../lib/utility";
 import {Day} from "../types/Day";
+import clone from "lodash/cloneDeep";
 
 type OperationName = 'acc'|'jmp'|'nop';
 
@@ -9,12 +10,46 @@ class BootSequence {
   private currentInstruction = 0;
   private visitedInstructions: Set<number> = new Set();
   private allInstructions:[OperationName,number][];
+  private modifiedInstructions!:[OperationName,number][];
+  private lastModifiedInstruction?:number;
+  private initialAccumulator = 0;
   private accumulator = 0;
 
   constructor(sequence:string){
     this.allInstructions = splitOnLinebreak(sequence)
       .map(instruction=>instruction.trim().split(' ') as [string,string])
       .map(inst=>[inst[0] as OperationName,Number(inst[1])]);
+    this.runProgram();
+    this.initialAccumulator = this.accumulator;
+    while(this.isCorrupted){
+      this.runProgram();
+    }
+  }
+
+  runProgram(){
+    this.currentInstruction = 0;
+    this.visitedInstructions.clear();
+    this.accumulator = 0;
+    this.modifiedInstructions = clone(this.allInstructions);
+    if(typeof this.lastModifiedInstruction == 'number'){
+      // Find the next instruction to modify
+      for(let i=this.lastModifiedInstruction+1; i<this.allInstructions.length; i++){
+        const [method,value] = this.modifiedInstructions[i];
+        if(method=='acc'){
+          continue;
+        }
+        if(method=='nop' && value==0){
+          // Swapping would create infinite loop
+          continue;
+        }
+        this.modifiedInstructions[i][0] = method=='nop' ? 'jmp' : 'nop';
+        this.lastModifiedInstruction = i;
+        break;
+      }
+    }
+    else{
+      this.lastModifiedInstruction = -1;
+    }
     try{
       for(const [method,value] of this.instructions()){
         this[method](value);
@@ -23,11 +58,16 @@ class BootSequence {
     catch{}
   }
 
+  get initialAccumulated(){ return this.initialAccumulator;}
   get accumulated(){ return this.accumulator; }
+  get isCorrupted(){
+    // Only valid if final index is one past the last instruction
+    return this.currentInstruction != this.allInstructions.length;
+  }
 
   *instructions(){
     while(true){
-      yield this.allInstructions[this.currentInstruction];
+      yield this.modifiedInstructions[this.currentInstruction];
     }
   }
 
@@ -49,20 +89,15 @@ class BootSequence {
   }
 }
 
-function parseDataset (string:string){
-  const lines = splitOnLinebreak(string);
-  return lines;
-}
-
 /** Tally the number of VALID passwords */
 function puzzle1(dataset:string){
   const game = new BootSequence(dataset);
-  return game.accumulated;
+  return game.initialAccumulated;
 }
 
 function puzzle2(dataset:string){
-  const data = parseDataset(dataset);
-  return -Infinity;
+  const game = new BootSequence(dataset);
+  return game.accumulated;
 }
 
 const day: Day = {
@@ -80,7 +115,7 @@ const day: Day = {
       acc +6
     `,
     puzzle1: 5,
-    puzzle2: Infinity
+    puzzle2: 8
   },
   puzzle1,
   puzzle2,
